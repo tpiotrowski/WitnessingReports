894399de18c03f19e63b0fd99c59b9dadb479e3c
Tomasz <piotrowski.kat@gmail.com>
2019-02-23 20:12:47 +0100

    First commit

---
+++ src/Witnessing.Client/AuthenticationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Witnessing.Client
+{
+    public class AuthenticationResult
+    {
+        public string AccessToken { get; set; }
+        public string  Uid { get; set; }
+        public string Client { get; set; }
+    }
+}
---
+++ src/Witnessing.Client/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Witnessing.Client
+{
+    public class AuthenticationService
+    {
+        public AuthenticationResult Login(string login, string password)
+        {
+
+            return new AuthenticationResult();
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
